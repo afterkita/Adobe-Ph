db8062d0e7c6ae195075f397a67339ec6459f923
Aerac <NVTsyplenkov@stud.kantiana.ru>
2022-05-31 20:58:06 +0200

    Сырая версия, для работы менять пути

---
+++ Form1.Designer.cs
@@ -0,0 +1,212 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(305, 25);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(594, 411);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 48);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(173, 30);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Обновить изображение";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(173, 30);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Загрузить изображение";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.Highlight;
+            this.button1.Location = new System.Drawing.Point(12, 108);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(129, 35);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Negative";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.button4.Location = new System.Drawing.Point(13, 149);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(128, 32);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Greey GR";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.button5.Location = new System.Drawing.Point(13, 187);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(128, 31);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "Miror Dominate Color";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.button6.Location = new System.Drawing.Point(13, 224);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(128, 31);
+            this.button6.TabIndex = 7;
+            this.button6.Text = "WaterMark";
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.button7.Location = new System.Drawing.Point(13, 261);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(128, 31);
+            this.button7.TabIndex = 8;
+            this.button7.Text = "Encrypt";
+            this.button7.UseVisualStyleBackColor = false;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.button8.Location = new System.Drawing.Point(147, 261);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(128, 31);
+            this.button8.TabIndex = 9;
+            this.button8.Text = "Decipher";
+            this.button8.UseVisualStyleBackColor = false;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(12, 364);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(173, 30);
+            this.button9.TabIndex = 10;
+            this.button9.Text = "Сохранить изображение";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(13, 416);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(172, 20);
+            this.textBox3.TabIndex = 13;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 400);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(149, 13);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "Путь к сохранённому файлу";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(925, 486);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.pictureBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Adobe Phoooonk";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var filePath = this.pictureBox1.ImageLocation;
+            filePath = Environment.CurrentDirectory + "copy1.bmp";
+            //this.pictureBox1.Image = new Bitmap(filePath);
+
+            FileStream fs = new System.IO.FileStream(filePath, FileMode.Open, FileAccess.Read);
+            pictureBox1.Image = Image.FromStream(fs);
+            this.pictureBox1.ImageLocation = filePath;
+            fs.Close();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            var filePath = string.Empty;
+            var filePath2 = string.Empty;
+
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.InitialDirectory = "c:\\";
+                openFileDialog.Filter = "bmp files (*.bmp)|*.bmp|All files (*.*)|*.*";
+                openFileDialog.FilterIndex = 1;
+                openFileDialog.RestoreDirectory = true;
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+
+                    filePath = openFileDialog.FileName;
+                    filePath2 = Environment.CurrentDirectory;
+                    
+                    File.Copy(filePath,filePath2 + "copy1.bmp", true);
+
+                    FileStream fs = new System.IO.FileStream(filePath, FileMode.Open, FileAccess.Read);
+                    pictureBox1.Image = Image.FromStream(fs);
+                    this.pictureBox1.ImageLocation = filePath;
+                    fs.Close();
+
+
+                }
+            }
+        }
+        [DllImport("my_dll.dll", CallingConvention = CallingConvention.Cdecl  )]
+        public static extern void Negative();
+        //public static extern int Addiction(int a, int b);
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Negative();
+            //int c = 3;//Addiction(3, 5);
+            //button1.Text = c.ToString();
+            var filePath = this.pictureBox1.ImageLocation;
+            filePath = Environment.CurrentDirectory + "copy1.bmp";
+            //this.pictureBox1.Image = new Bitmap(filePath);
+
+            FileStream fs = new System.IO.FileStream(filePath, FileMode.Open, FileAccess.Read);
+            pictureBox1.Image = Image.FromStream(fs);
+            this.pictureBox1.ImageLocation = filePath;
+            fs.Close();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+        [DllImport("my_dll.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern void BandW();
+        private void button4_Click(object sender, EventArgs e)
+        {
+            BandW();
+            BandW();
+            BandW();
+            BandW();
+            var filePath = this.pictureBox1.ImageLocation;
+            filePath = Environment.CurrentDirectory + "copy1.bmp";
+            FileStream fs = new System.IO.FileStream(filePath, FileMode.Open, FileAccess.Read);
+            pictureBox1.Image = Image.FromStream(fs);
+            this.pictureBox1.ImageLocation = filePath;
+            fs.Close();
+        }
+        [DllImport("my_dll.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern void MirorColor();
+        private void button5_Click(object sender, EventArgs e)
+        {
+            MirorColor();
+            var filePath = this.pictureBox1.ImageLocation;
+            filePath = Environment.CurrentDirectory + "copy1.bmp";
+            FileStream fs = new System.IO.FileStream(filePath, FileMode.Open, FileAccess.Read);
+            pictureBox1.Image = Image.FromStream(fs);
+            this.pictureBox1.ImageLocation = filePath;
+            fs.Close();
+        }
+        [DllImport("my_dll.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern void WaterMark();
+        private void button6_Click(object sender, EventArgs e)
+        {
+            WaterMark();
+            var filePath = this.pictureBox1.ImageLocation;
+            filePath = Environment.CurrentDirectory + "copy1.bmp";
+            FileStream fs = new System.IO.FileStream(filePath, FileMode.Open, FileAccess.Read);
+            pictureBox1.Image = Image.FromStream(fs);
+            this.pictureBox1.ImageLocation = filePath;
+            fs.Close();
+        }
+        [DllImport("my_dll.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern void Encrypt();
+        private void button7_Click(object sender, EventArgs e)
+        {
+            Encrypt();
+            var filePath = this.pictureBox1.ImageLocation;
+            filePath = Environment.CurrentDirectory + "copy1.bmp";
+            FileStream fs = new System.IO.FileStream(filePath, FileMode.Open, FileAccess.Read);
+            pictureBox1.Image = Image.FromStream(fs);
+            this.pictureBox1.ImageLocation = filePath;
+            fs.Close();
+        }
+        [DllImport("my_dll.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern void Decipher();
+        private void button8_Click(object sender, EventArgs e)
+        {
+            Decipher();
+            var filePath = this.pictureBox1.ImageLocation;
+            filePath = Environment.CurrentDirectory + "copy1.bmp";
+            FileStream fs = new System.IO.FileStream(filePath, FileMode.Open, FileAccess.Read);
+            pictureBox1.Image = Image.FromStream(fs);
+            this.pictureBox1.ImageLocation = filePath;
+            fs.Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            var filePath = string.Empty;
+            FolderBrowserDialog folderBrowserDialog1 = new FolderBrowserDialog();
+            string s;
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                
+                s = folderBrowserDialog1.SelectedPath;
+                textBox3.Text = folderBrowserDialog1.SelectedPath;
+            }
+            
+            File.Copy(this.pictureBox1.ImageLocation, textBox3.Text+"\\saved.bmp", true);
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
